e8e94d26d356b1d34d7220730ec00180ee720b18
Tien <doanvitien2628@gmail.com>
2026-05-25 12:59:41 +0700

    Add background task, setTimeout and logout button

--- src/components/ConfigPanel.jsx
+++ src/components/ConfigPanel.jsx
@@ -50,8 +50,9 @@ export default function ConfigPanel({ colorMode, setColorMode, selectedColorId, 
       if (res.status === 401) throw new Error("401"); 
       if (!res.ok) throw new Error("Lỗi hệ thống");
     })
-    .then(() => {
-      alert("Đã tạo lịch thành công!");
+    .then(async() => {
+      await new Promise(resolve => {setTimeout(resolve, 25000)});
+      alert("Đã tạo lịch thành công! Nếu lịch vẫn chưa xuất hiện, bạn hãy reload lại trang Google Calendar sau ít phút nhé");
     })
     .catch(async(error) => {
       console.error("Lỗi:", error);
@@ -220,40 +221,59 @@ export default function ConfigPanel({ colorMode, setColorMode, selectedColorId, 
       </div>
 
         <div className="mt-auto">
-          <button 
-            className={`cursor-pointer w-full font-bold py-3.5 px-6 rounded-xl transition-all shadow-md flex items-center justify-center gap-2
-              ${isLoading 
-                ? 'bg-slate-400 text-slate-100 cursor-not-allowed' 
-                : 'bg-slate-800 hover:bg-slate-700 text-white hover:shadow-lg focus:ring-4 focus:ring-slate-200'
-              }`}
-            onClick={async() => {
-              if (!isLoggedIn) {
-                
-                const success = await handleLogin(); 
-
-                if (success) {
-                  
-                  setIsLoading(true); 
-                  await handleCreateSchedule(); 
+          <div className='flex flex-row gap-2'>
+            {isLoggedIn && (
+              <button className='cursor-pointer w-full font-bold py-3 px-5.5 rounded-xl transition-all shadow-md flex items-center justify-center gap-2
+              bg-white text-slate-800 border border-slate-200 hover:bg-gray-100'
+              onClick={()=>{
+                if(localStorage.getItem("isLoggedIn") === "true"){
+                  localStorage.removeItem("isLoggedIn");
+                  localStorage.removeItem("access_token");
+                  localStorage.removeItem("refresh_token");
+                  setIsLoggedIn(false);
+                  alert("Bạn đã đăng xuất thành công!");
                 }
-              } else {
-                await handleCreateSchedule();
-              }
-              }}
-            disabled={isLoading} 
-          >
-            {isLoading ? (
-              <>
-                <svg className="animate-spin -ml-1 mr-2 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
-                Đang tạo lịch...
-              </>
-            ) : (
-              "Tạo lịch"
+
+              }}>
+                Đăng xuất
+              </button>
             )}
-          </button>
+            <button 
+              className={`cursor-pointer w-full font-bold py-3.5 px-6 rounded-xl transition-all shadow-md flex items-center justify-center gap-2
+                ${isLoading 
+                  ? 'bg-slate-400 text-slate-100 cursor-not-allowed' 
+                  : 'bg-slate-800 hover:bg-slate-700 text-white hover:shadow-lg focus:ring-4 focus:ring-slate-200'
+                }`}
+              onClick={async() => {
+                if (!isLoggedIn) {
+                  
+                  const success = await handleLogin(); 
+
+                  if (success) {
+                    
+                    setIsLoading(true); 
+                    await handleCreateSchedule(); 
+                  }
+                } else {
+                  await handleCreateSchedule();
+                }
+                }}
+              disabled={isLoading} 
+            >
+              {isLoading ? (
+                <>
+                  <svg className="animate-spin -ml-1 mr-2 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+                  Đang tạo lịch...
+                </>
+              ) : (
+                "Tạo lịch"
+              )}
+            </button>
+            
+          </div>
           <p className="text-xs text-slate-400 text-center mt-2 italic leading-relaxed">
               *Lưu ý là lịch sẽ được lưu trên Google Calendar của tài khoản Google bạn đã đăng nhập nhé
           </p>
